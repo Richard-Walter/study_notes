720af1612a9b29539cab2ba35b59375ac51be192
Richard-Walter <willthisnamedo@gmail.com>
2019-07-05 20:32:54 +1000

    bug fix - duplilcation of note subjects collapsing

--- scripts/html-generator.js
+++ scripts/html-generator.js
@@ -1,6 +1,7 @@
 const generateNoteTemplate = (note, subject, note_id) => {
 
     subject = document.getElementById(subject)
+    
     note_list = subject.querySelector('.notes')
 
     const html = `
@@ -30,18 +31,20 @@ const generatePinnedNoteTemplate = (note, note_id) => {
 
 const generateSubjectTemplate = (bgColor, collapse, subject) => {
 
+    subject_nospace = subject.split(' ').join('_')
+
     const html = `
 
     <section class="py-1 my-4 rounded container-fluid" style="background:${bgColor}" id="${subject}">
     <header class="subject-header text-center text-light border-bottom border-light mb-2 ">
         <i class="my-color-picker fas fa-palette fa-lg float-left p-1"></i>
-        <a class="badge" data-toggle="collapse" href="#collapse1" role="button" aria-expanded="false"
+        <a class="badge" data-toggle="collapse" href="#collapse${subject_nospace}" role="button" aria-expanded="false"
             aria-controls="collapseExample">
             <h5 class="d-inline text-center text-light">${subject}</h5>
         </a>
         <i class="subject-delete far fa-window-close fa-lg float-right p-1"></i>
     </header>
-    <div id="collapse1" class="collapse show">
+    <div id="collapse${subject_nospace}" class="collapse show">
         <ul class="notes list-group mb-2 mx-auto text-dark small ">
 
         </ul>
