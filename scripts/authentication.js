fc259aeaa0df8e1853bf2b0f1f576b27b00cda9b
Richard-Walter <willthisnamedo@gmail.com>
2019-07-06 21:59:31 +1000

    add basic sign up sign in functionality

---
+++ scripts/authentication.js
@@ -0,0 +1,75 @@
+//**** SIGN IN ******/
+
+const signupForm = document.querySelector('#signup-form')
+
+// event listener is submit rather than on button as it listens to return as well
+signupForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const email = signupForm['signup-email'].value
+    const password = signupForm['signup-password'].value
+    const name = signupForm['signup-name'].value
+
+
+    // sign up the user using firebase auth object we created in the HTML
+    //this is asyncronous and returns a promise containing a user credential
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+
+        console.log(cred);
+
+        //create a new document in users collection (firebase will create if doesn't exist)
+        //then instead of add() we use doc() where we can create out own ID(userID)
+        //it then returns a promise
+        // return db.collection('users').doc(cred.user.uid).set({
+        //     bio: signupForm['signup-bio'].value,
+        //     displayName: name
+        // })
+
+    }).then(() => {
+
+               
+        // $('#modalSignup').hide();
+        $("#modalSignup").modal("toggle");
+        
+        signupForm.reset()
+        // signupForm.querySelector('.error').innerHTML = ''
+
+    }).catch((err) => {
+        signupForm.querySelector('.error').innerHTML = err.message
+    })
+})
+
+//**** LOG IN ******/
+const loginForm = document.querySelector('#login-form')
+loginForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const email = loginForm['login-email'].value
+  const password = loginForm['login-password'].value
+
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+
+    console.log(cred);
+
+    //close the signup modal
+    // $('#modalLogin').hide();
+    $("#modalLogin").modal("toggle");
+
+    loginForm.reset()
+
+    // loginForm.querySelector('.error').innerHTML = ''
+  }).catch((err) => {
+    loginForm.querySelector('.error').innerHTML = err.message
+ })
+
+})
+
+
+//**** LOG OUT ******/
+const logout = document.querySelector('#logout')
+logout.addEventListener('click', e => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+      console.log("User has logged out");
+  })
+})
